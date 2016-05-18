c6cdecaa2734ca1722a6f3324639acf3af484ef7
radx <radx.plugin@gmail.com>
2016-05-18 16:12:08 +0200

    Removed test

--- samples/Xcl.Samples/EditSamples.cs
+++ samples/Xcl.Samples/EditSamples.cs
@@ -23,11 +23,6 @@ namespace EditSamples
 		{
 			base.Loaded ();
 
-			var edit2 = TButton.Create (self);
-			edit2.Parent = self;
-			edit2.Caption = "Button1";
-			edit2.Color = TColor.clRed;
-
 			edit = TEdit.Create (self);
 			edit.Parent = self;
 			edit.Left = 10;
